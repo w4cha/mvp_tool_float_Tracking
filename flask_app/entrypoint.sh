e45dc647e5f9904e6e47460210287e9160efb594
Lucas Folch <folch.lucas@gmail.com>
2026-03-22 21:51:54 -0300

    port

--- flask_app/entrypoint.sh
+++ flask_app/entrypoint.sh
@@ -9,4 +9,4 @@ python flask_app/init_db.py
 # --bind 0.0.0.0:$PORT tells Gunicorn to use Render's dynamic port
 # --chdir flask_app enters your app folder before running
 echo "Starting Web Server..."
-exec gunicorn --bind 0.0.0.0:$PORT --chdir ./flask_app app:app
+exec gunicorn --bind 0.0.0.0:10000 --chdir ./flask_app app:app
